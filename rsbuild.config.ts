4e627cbb219e6b13a51e644d57cd23c939520741
uncaught <uncaught42@gmail.com>
2024-03-29 14:29:15 +0100

    first commit

---
+++ rsbuild.config.ts
@@ -0,0 +1,22 @@
+import {defineConfig} from '@rsbuild/core';
+import {pluginReact} from '@rsbuild/plugin-react';
+
+export default defineConfig({
+  plugins: [pluginReact()],
+  tools: {
+    swc: {
+      jsc: {
+        experimental: {
+          plugins: [
+            [
+              '@swc/plugin-styled-jsx',
+              {
+                plugins: ['@styled-jsx/plugin-sass'],
+              },
+            ],
+          ],
+        },
+      },
+    },
+  },
+});
